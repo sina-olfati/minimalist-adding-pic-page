0fae834622befb100733ad0895dadfd8ec96e26d
sina <sinaolfati5@gmail.com>
2022-02-09 12:30:06 +0330

    making it capable of adding packs with images and names

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-var animals = ["images/dog.png", "images/cat.png", "images/eagle.png", "images/shrek.png", "images/jelly-fish.png", "images/owl.png",]
-var a = document.getElementById("url-input")
+// adding urls to input with clicking the animal name
+var animals = ["images/dog.png", "images/cat.png", "images/eagle.png", "images/shrek.png", "images/jelly-fish.png", "images/owl.png", "images/bird.png"]
+var url = document.getElementById("url-input")
 
 
 function addUrl(x) {
     if (x==1){
-    a.value = animals[0]
+        url.value = animals[0]
     }else if(x==2){
-        a.value = animals[1]
+        url.value = animals[1]
     }else if(x==3){
-        a.value = animals[2]
+        url.value = animals[2]
     }else if(x==4){
-        a.value = animals[3]
+        url.value = animals[3]
     }else if(x==5){
-        a.value = animals[4]
+        url.value = animals[4]
     }else if(x==6){
-        a.value = animals[5]
+        url.value = animals[5]
+    }else if(x==7){
+        url.value = animals[6]
     }
+}
+
+
+// adding the image pack with clicking add button
+var container = document.getElementById("section-img-packs")
+var names = document.getElementById("name-input").value
+
+function addImgPack(){
+    var addDiv = document.createElement("div")
+    container.appendChild(addDiv)
+    var addImg = document.createElement("img")
+    addImg.src = url.value
+    addDiv.appendChild(addImg)
+    var addh4 = document.createElement("h4")
+    addh4.innerText = names.value
+    addDiv.appendChild(addh4)
 }
